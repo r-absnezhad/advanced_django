d7c212b991771cfed2d65bfe3052158b7e64fa74
r-absnezhad <167188241+r-absnezhad@users.noreply.github.com>
2024-10-26 18:33:16 +0330

    completing docker compose
    some changes related to docker compose

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,9 @@ WORKDIR /app
 
 COPY requirements.txt /app/
 
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
 COPY ./core /app/
 
-CMD [ "python3","manage.py","runserver","0,0,0,0:8000"]
+#CMD [ "python3","manage.py","runserver","0,0,0,0:8000"]
--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-#from decouple import config
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-98phn04co4260ac=2z#rx@e5(=)+&(s&okx9ll1c%e=h4j4n^_'
+SECRET_KEY = config('SECRET_KEY')#rx@e5(=)+&(s&okx9ll1c%e=h4j4n^_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')], default="*")
 
 
 # Application definition
